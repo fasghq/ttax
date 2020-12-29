a002ed39aa04a936c82286807c7397f447a3672d
Alexander Novikov <sasha.v.novikov@gmail.com>
2020-12-29 12:08:45 +0000

    init TTMatrix class

--- ttax/base_class.py
+++ ttax/base_class.py
@@ -3,9 +3,7 @@ import jax.numpy as jnp
 import flax
 
 
-@flax.struct.dataclass
-class TT:
-  tt_cores: List[jnp.array]
+class TTBase:
 
   def __mul__(self, other):
     # We can't import ops in the beginning since it creates cyclic dependencies.
@@ -13,8 +11,17 @@ class TT:
     return ops.multiply(self, other)
 
   @property
+  def axis_dim(self):
+    return self.num_batch_dims + 1
+
+  @property
   def batch_shape(self):
     return self.tt_cores[0].shape[:self.num_batch_dims]
+
+
+@flax.struct.dataclass
+class TT(TTBase):
+  tt_cores: List[jnp.array]
 
   @property
   def shape(self):
@@ -22,9 +29,14 @@ class TT:
     return tuple(list(self.batch_shape) + no_batch_shape)
 
   @property
-  def axis_dim(self):
-    return self.num_batch_dims + 1
+  def num_batch_dims(self):
+    return len(self.tt_cores[0].shape) - 3
+
+
+@flax.struct.dataclass
+class TTMatrix(TTBase):
+  tt_cores: List[jnp.array]
 
   @property
   def num_batch_dims(self):
-    return len(self.tt_cores[0].shape) - 3
+    return len(self.tt_cores[0].shape) - 4
