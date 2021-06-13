88816e4bdd03bd0cbb4ec07578f6a88a6a0082f2
fasghq <dmitrybel9@gmail.com>
2021-06-13 15:34:53 +0300

    modify conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../t3f'))
+sys.path.insert(0, os.path.abspath('../ttax'))
 
 
 # -- General configuration ------------------------------------------------
