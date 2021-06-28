df69897f4bbb86de00b1076befe38dafcfc5e8ad
Alexander Novikov <sasha.v.novikov@gmail.com>
2021-06-28 15:41:04 +0100

    Add double projection test

--- ttax/autodiff_test.py
+++ ttax/autodiff_test.py
@@ -31,5 +31,27 @@ class AutodiffTest(jtu.JaxTestCase):
     desired = tensor
     self.assertAllClose(ops.full(actual), ops.full(desired), rtol=1e-4)
 
+  def testDoubleProjection(self):
+    """Compare P grad f(x) against P grad (<x, stop_grad(P grad f(x))>)."""
+    dtype = jnp.float32
+    rng = jax.random.PRNGKey(42)
+    vector = random_.matrix(rng, ((2, 1, 3, 4), (1, 1, 1, 1)),
+                            tt_rank=[1, 2, 4, 3, 1], dtype=dtype)
+    matrix = random_.matrix(rng, ((2, 1, 3, 4), (2, 1, 3, 4)),
+                            tt_rank=[1, 2, 4, 3, 1], dtype=dtype)
+
+    def f(x):
+      mat_vec = ops.matmul(matrix, x)
+      return ops.flat_inner(x, mat_vec)
+
+    grad_f = autodiff.grad(f)
+    grad = grad_f(vector)
+
+    def f2(x):
+      return ops.flat_inner(x, grad)
+
+    double_grad = autodiff.grad(f2)(vector)
+    self.assertAllClose(ops.full(double_grad), ops.full(grad), rtol=1e-4)
+
 if __name__ == '__main__':
   absltest.main(testLoader=jtu.JaxTestLoader())
