d8c6815fce4a5e654835a84e6c706b5cc9981b75
johanDDC <ivan,peshekhonov@gmail.com>
2021-08-22 17:44:25 +0300

    Fix hessian_vector_product and add test

--- ttax/autodiff_test.py
+++ ttax/autodiff_test.py
@@ -44,5 +44,22 @@ class AutodiffTest(jtu.JaxTestCase):
     double_project = autodiff.project(project, vector)
     self.assertAllClose(ops.full(project), ops.full(double_project), rtol=1e-4)
 
+  def testHessianVectorProduct(self):
+      rng1, rng2, rng3 = jax.random.split(jax.random.PRNGKey(0), 3)
+      dtype = jnp.float32
+      shape = (5, 5, 5)
+      A = random_.matrix(rng1, (shape, shape), dtype=dtype)
+      AT = ops.transpose(A)
+      A_plus_AT = A + AT
+      x = random_.tensor(rng2, shape, dtype=dtype)
+      vec = random_.tensor(rng3, shape, dtype=dtype)
+      proj_vec = autodiff.project(vec, x)
+
+      func = lambda x: ops.flat_inner(x, ops.matrix_tensor_matmul(A, x))
+      desired = autodiff.project(ops.matrix_tensor_matmul(A_plus_AT, proj_vec), x)
+      desired = ops.full(desired)
+      actual = ops.full(autodiff.hessian_vector_product(func)(x, vec))
+      self.assertAllClose(desired, actual, rtol=1e-4)
+
 if __name__ == '__main__':
   absltest.main(testLoader=jtu.JaxTestLoader())
